2977c96f1735e3dbdd539663ed3736017ec79748
MFG <macfglenn@gmail.com>
2022-03-08 21:24:55 -0500

    routes seem to work, time to test

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,7 +28,7 @@ router.get("/:id", (req, res) => {
     },
     include: {
       model: Product,
-      attributes: ["product-name", "price", "stock", "category_id"],
+      attributes: ["product_name", "price", "stock", "category_id"],
     },
   })
     .then((dbTagData) => res.json(dbTagData))
